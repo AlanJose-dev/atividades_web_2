f0a981a26c43324b1e6c6e422666a166b5f7d015
Alan José <alanjsdelima@gmail.com>
2024-12-03 22:03:57 -0300

    Adicionando atividade 5.5

--- atividade_05/routes/web.php
+++ atividade_05/routes/web.php
@@ -15,3 +15,12 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('categories', \App\Http\Controllers\CategoryController::class);
+
+Route::get('/books/create-id-number', [\App\Http\Controllers\BookController::class, 'createWithId'])->name('books.create.id');
+Route::post('/books/create-id-number', [\App\Http\Controllers\BookController::class, 'storeWithId'])->name('books.store.id');
+
+Route::get('/books/create-select', [\App\Http\Controllers\BookController::class, 'createWithSelect'])->name('books.create.select');
+Route::post('/books/create-select', [\App\Http\Controllers\BookController::class, 'storeWithSelect'])->name('books.store.select');
+
+// Rotas RESTful para index, show, edit, update, delete (tem que ficar depois das rotas /books/create-id-number e /books/create-select)
+Route::resource('books', \App\Http\Controllers\BookController::class)->except(['create', 'store']);
