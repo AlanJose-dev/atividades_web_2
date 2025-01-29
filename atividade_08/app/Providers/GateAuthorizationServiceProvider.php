558ac23bcf92ac30d4ab4d4dcebc1286461dc7a6
Alan José <alanjsdelima@gmail.com>
2025-01-28 22:53:41 -0300

    Adicionando e registrando a policy de Category

--- atividade_08/app/Providers/GateAuthorizationServiceProvider.php
+++ atividade_08/app/Providers/GateAuthorizationServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use App\Models\Book;
+use App\Models\Category;
 use App\Models\Publisher;
 use App\Policies\BookPolicy;
+use App\Policies\CategoryPolicy;
 use App\Policies\PublisherPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -26,5 +28,6 @@ class GateAuthorizationServiceProvider extends ServiceProvider
     {
         Gate::policy(Book::class, BookPolicy::class);
         Gate::policy(Publisher::class, PublisherPolicy::class);
+        Gate::policy(Category::class, CategoryPolicy::class);
     }
 }
